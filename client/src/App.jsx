b36bf9eedd77aa754e8a669f22038e30e3f55391
rbrignoli <ramon.brignoli@gmail.com>
2024-02-07 21:56:38 -0300

    client feat: add init pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
