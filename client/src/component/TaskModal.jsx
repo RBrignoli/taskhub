e98bf4dbca6530f158d63bb55052aaf94458b3ba
rbrignoli <ramon.brignoli@gmail.com>
2024-03-09 14:26:59 -0300

    client feat: add comments

--- client/src/component/TaskModal.jsx
+++ client/src/component/TaskModal.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { mockedCommentsData } from "../assets/mocks/projectmocks";
 import apiService from "../services/api";
 import API_URLS from "../services/server-urls";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const fetchComments = async (setComments, task) => {
   try {
-    setComments(mockedCommentsData); //TODO dinamically add comments
+    const response = await apiService.get(API_URLS.comments + task.id);
+    setComments(response);
   } catch (error) {
     console.error("Error fetching comments:", error);
     throw error;
@@ -17,14 +19,35 @@ const TaskModal = ({ isOpen, onClose, task }) => {
   const [comments, setComments] = useState([]);
   const [hoursSpent, setHoursSpent] = useState(task.hoursspent);
 
-  const handleAddComment = () => {
+  const handleAddComment = async () => {
     console.log(commentContent);
+    try {
+      await apiService.api(
+        JSON.stringify({'task_id': task.id, 'content': commentContent}),
+        API_URLS.comments,
+        "POST"
+      );
+      fetchComments(setComments, task);
+    } catch (error) {
+      alert("Error creating comment. Please try again later.");
+    }
+  };
+  const handleDeleteComment = (comment) => async () => {
+    console.log(comment);
+    if (window.confirm("Are you sure you want to delete this comment?")) {
+      try {
+        await apiService.api("", API_URLS.comments + comment._id, "DELETE");
+        fetchComments(setComments, task);
+      } catch (error) {
+        alert("Error deleting comment. Please try again later.");
+      }
+    }
   };
 
   const handleAddhours = async () => {
     try {
       const response = await apiService.api(
-        JSON.stringify({'hoursspent': hoursSpent}),
+        JSON.stringify({ hoursspent: hoursSpent }),
         API_URLS.edittask + task.id,
         "POST"
       );
@@ -40,6 +63,7 @@ const TaskModal = ({ isOpen, onClose, task }) => {
   useEffect(() => {
     fetchComments(setComments, task);
   }, [task]);
+
   const modalOverlayStyle = isOpen
     ? "opacity-100 pointer-events-auto"
     : "opacity-0 pointer-events-none";
@@ -87,8 +111,12 @@ const TaskModal = ({ isOpen, onClose, task }) => {
                 {comments.map((comment) => (
                   <div key={comment.id} className="mb-2">
                     <div>{comment.content}</div>
-                    <div className="text-gray-500 text-sm">
-                      {comment.user.name} at {comment.createdAt.date}
+                    <div className="text-gray-500 text-sm flex">
+                      {comment.user.name} at {comment.createdAt}
+                      <AiOutlineDelete
+                        className="w-4 h-4 bg-red-500 mx-2 my-1"
+                        onClick={handleDeleteComment(comment)}
+                      />
                     </div>
                   </div>
                 ))}
